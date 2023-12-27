7c481ca17ad54d48975c211ac8f54db7e516005d
sudharsandeveloper <sudharsandeveloper@users.noreply.github.com>
2023-12-28 00:19:53 +0530

    comments only added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app  = express();
-
 const path = require('path');
 
+// for make public as a static location
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/register',(req,res) => {
@@ -12,7 +12,7 @@ app.get('/login',(req,res) => {
     res.sendFile(path.join(__dirname,'./view/Admin/auth', 'login.html'));
 })
 
-
+// server run
 app.listen(3000, () => {
     console.log(`Server is listening at http://localhost:${3000}`);
   });
