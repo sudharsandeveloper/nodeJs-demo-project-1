8fff55f75bbf1a13c17a5bcb9481e344ab89f3ed
sudharsandeveloper <sudharsandeveloper@users.noreply.github.com>
2024-01-01 23:33:46 +0530

    user login added. just check funtionality

--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,60 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const router = express.Router();
 
 const { User } = require("../models");
 
-const router = express.Router();
-router.get("/",(req,res)=>{
-    res.send("User route is displaying data");
-})
+    router.get("/",(req,res)=>{
+        res.send("User route is displaying data");
+    })
 
-router.get('/register',(req,res) => {
-    // res.sendFile(path.join(__dirname,'./view/Admin/auth', 'register.html'));
-    res.render('register');
-})
+    router.get('/register',(req,res) => {
+        // res.sendFile(path.join(__dirname,'./view/Admin/auth', 'register.html'));
+        res.render('register');
+    })
 
-router.post('/register', async (req, res) => {
-    // const { first_name: firstName, last_name: lastName, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const userDetails = {
-      firstName: req.body.first_name,
-      lastName: req.body.last_name,
-      email: req.body.email,
-      password: await bcrypt.hash(req.body.password, salt)
-    }
-    // const passwordHashed = bcrypt.hash(password, salt)
-    // console.log(userDetails);
-    try {
-      // console.log(firstName, lastName, email, password);
-      const newUser = await User.create(userDetails);
-      // console.log('User registered:', newUser.email);
-      res.send('Registration successful!');
-    } catch (error) {
-      console.error('Error during registration:', error.message);
-      res.status(500).send('Registration failed.');
-    }
-  });
+    router.post('/register', async (req, res) => {
+        // const { first_name: firstName, last_name: lastName, email, password } = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const userDetails = {
+        firstName: req.body.first_name,
+        lastName: req.body.last_name,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, salt)
+        }
+        // const passwordHashed = bcrypt.hash(password, salt)
+        // console.log(userDetails);
+        try {
+        // console.log(firstName, lastName, email, password);
+        const newUser = await User.create(userDetails);
+        // console.log('User registered:', newUser.email);
+        res.redirect('/user/login');
+        } catch (error) {
+        console.error('Error during registration:', error.message);
+        res.status(500).send('Registration failed.');
+        }
+    });
+
+    router.get('/login',(req,res) => {
+        res.render('login');
+    })
+
+    router.post('/login',async (req, res) => {
+        const { email, password } = req.body;
+        // console.log(email, password);
+        const user = await User.findOne({ where : {email : req.body.email }});
+        // console.log(user);
+        if(user){
+            const password_valid = await bcrypt.compare(req.body.password,user.password);
+            if(password_valid){
+                res.redirect('/user');
+            }else{
+                res.redirect('/user/login');
+            }
+        }else{
+            res.send('Invalide user');
+        }
+
+    })
 
 module.exports=router;
